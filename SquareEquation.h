2ace9e7b3832abd890deab228c41a7b44a18ad57
Roman Loginov <logipamar@yandex.ru>
2018-09-13 09:59:40 +0300

    Options -v and -p implemented

--- SquareEquation.h
+++ SquareEquation.h
@@ -19,14 +19,20 @@
 #include <vector>
 #include <cassert>
 #include <cmath>
+#include <iostream>
 
 /*!
  * Constant to indicate infinite number of roots.
  */
 const int INFINITE_ROOTS = -1;
 
-int solveNontrivialSquareEquation(double a, double b, double c, double* root1, double* root2);
-int solveLinearEquation(double a, double b, double* root);
+/*!
+ * Allows to make debug output more concise
+ */
+#define COUT if (needVerbose) std::cout
+
+int solveNontrivialSquareEquation(double a, double b, double c, double* root1, double* root2, bool needVerbose);
+int solveLinearEquation(double a, double b, double* root, bool needVerbose);
 
 /*!
  * Main solver function.<br>
@@ -38,18 +44,26 @@ int solveLinearEquation(double a, double b, double* root);
  * @param c Free coefficient
  * @param root1 Pointer to the first root
  * @param root2 Pointer to the second root
+ * @param needVerbose - Is huge output provided
  * @return Number of roots found
  */
-int solveEquation(double a, double b, double c, double* root1, double* root2)
+int solveEquation(double a, double b, double c, double* root1, double* root2, bool needVerbose = false)
 {
     assert(root1);
     assert(root2);
     assert(root1 != root2);
-
+    
+    COUT << "I am square equation with coeffs: [" << a << ", " << b << ", " << c << "]\n";
     if (a != 0)
-        return solveNontrivialSquareEquation(a, b, c, root1, root2);
+    {
+        COUT << "A is not 0, that's why I am not trivial\n";
+        return solveNontrivialSquareEquation(a, b, c, root1, root2, needVerbose);
+    }
     else
-        return solveLinearEquation(b, c, root1);
+    {
+        COUT << "A is 0, so equation is in fact linear\n";
+        return solveLinearEquation(b, c, root1, needVerbose);
+    }
 }
 
 /*!
@@ -58,25 +72,33 @@ int solveEquation(double a, double b, double c, double* root1, double* root2)
  * Parameters and return value are similar with solveEquation()
  * @see solveEquation()
  */
-int solveNontrivialSquareEquation(double a, double b, double c, double* root1, double* root2)
+int solveNontrivialSquareEquation(double a, double b, double c, double* root1, double* root2, bool needVerbose = false)
 {
     assert(root1);
     assert(root2);
     assert(root1 != root2);
     assert(a != 0);
+    COUT << "Nontrivial equation solver called\n";
 
     double discriminant = b * b - 4 * a * c;
+    COUT << "Discriminant counted as D = " << discriminant << "\n";
 
     if (discriminant < 0)
     {
+        COUT << "Zero roots cause D < 0.\nThe end.\n";
         return 0;
     }
     
     *root1 = (-b + sqrt(discriminant)) / (2 * a);
+    COUT << "D >= 0. First root calculated: " << *root1 << "\n";
 
     if (discriminant > 0)
+    {
         *root2 = (-b - sqrt(discriminant)) / (2 * a);
-
+        COUT << "Moreover, D > 0, so second root calculated " << *root2 << "\n";
+    }
+    
+    COUT << "Finally, number of roots is " << 1 + (discriminant > 0);
     return 1 + (discriminant > 0);
 }
 
@@ -84,20 +106,30 @@ int solveNontrivialSquareEquation(double a, double b, double c, double* root1, d
  * Linear equation solver.<br>
  * Simply solves \f$ax + b = 0\f$ equation.<br>
  * @param a, b - coefficients of the equation
+ * @param needVerbose - Is huge ouput provided
  * @return Number of roots found. If it is infinite, returns #INFINITE_ROOTS
  */
-int solveLinearEquation(double a, double b, double* root)
+int solveLinearEquation(double a, double b, double* root, bool needVerbose)
 {
     assert(root);
+    COUT << "Linear equation solver called!\n";
 
     if (a == 0)
     {
+        COUT << "But that's not linear. It is c = 0\n";
         if (b == 0)
+        {
+            COUT << "0 = 0 is always true. Infinite number of roots!\n";
             return INFINITE_ROOTS; //Any value in root fits
+        }
         else
+        {
+            COUT << "When c is not 0, no roots can be found\n";
             return 0;
+        }
     }
     
     *root = -b / a;
+    COUT << "Single root " << *root << "\n";
     return 1;
 }
--- main.cpp
+++ main.cpp
@@ -1,17 +1,95 @@
 
-#include <stdio.h>
+#include <iostream>
+#include <iomanip>
 #include <unistd.h>
 #include <getopt.h>
 #include <stdlib.h>
+#include <string.h>
 #include "SquareEquation.h"
 
 const int DEFAULT_PRECISION = -1;
+const size_t MAX_ANSWER_SIZE = 256;
 
 struct Options
 {
     int precision_;
     bool needVerbose_;
 };
+
+Options getOptions(int argc, char** argv);
+bool needContinuation();
+
+int main(int argc, char** argv)
+{
+    Options options = getOptions(argc, argv);
+
+    double a = 0, b = 0, c = 0;
+    
+    printf("Hi! This is a v1.0 of SquareEquationSolver\n");
+
+    do
+    {
+        printf("Imagine your favourite ax^2 + bx + c = 0 equation and just enter a, b, c!\n"
+               "a: ");
+
+        scanf("%lf", &a);
+
+        printf("b: ");
+        scanf("%lf", &b);
+
+        printf("c: ");
+        scanf("%lf", &c);
+
+        double root1 = 0, root2 = 0;
+
+        int nRoots = solveEquation(a, b, c, &root1, &root2, options.needVerbose_);
+
+        printf("Your equation %gx^2 + %gx + %g = 0 has ", a, b, c);
+        
+        if (options.precision_ != DEFAULT_PRECISION)
+            std::cout << std::setprecision(options.precision_) << std::fixed;
+
+        if (nRoots == INFINITE_ROOTS)
+        {
+            printf("has infinite number of roots!\n"
+                   "Any number will fit the equation!\n");
+        }
+        else if (nRoots == 1)
+        {
+            std::cout << " 1 root. It is " << root1 << "\n";
+        }
+        else if (nRoots == 0)
+        {
+            printf(" no roots!\n");
+        }
+        else
+        {
+            std::cout << " 2 roots. They are " << root1 << " and " << root2 << "\n";
+        }
+    }
+    while (needContinuation());
+
+    printf("See you later!\n");
+}
+
+bool needContinuation()
+{
+    char answer[MAX_ANSWER_SIZE + 2] = "";
+    int nAttempts = 0;
+
+    while (strcmp(answer, "y") != 0 and strcmp(answer, "n") != 0)
+    {
+        if (nAttempts == 0)
+            printf("Do you need to try again? [y/n]\n");
+        else
+            printf("Please enter only 'y' or 'n'\n");
+        
+        scanf("%s", answer);
+        ++nAttempts;
+    }
+
+    return strcmp(answer, "y") == 0;
+}
 
 Options getOptions(int argc, char** argv)
 {
@@ -21,7 +99,6 @@ Options getOptions(int argc, char** argv)
                            {"verbose",   0, nullptr, 'v'},
                            {0,           0,       0,   0} };
     Options optionsGot = {DEFAULT_PRECISION, false}; 
-
     
     int opt = 0;
     while ((opt = getopt_long(argc, argv, possibleOptions, longOpt, nullptr)) != -1)
@@ -40,46 +117,4 @@ Options getOptions(int argc, char** argv)
     }
 
     return optionsGot;
-}
-
-int main(int argc, char** argv)
-{
-    Options options = getOptions(argc, argv);
-    printf("Precision: %d, Verbose: %d\n", options.precision_, options.needVerbose_);
-
-    double a = 0, b = 0, c = 0;
-    
-    printf("Hi! This is a v1.0 of SquareEquationSolver\n"
-           "Imagine your favourite ax^2 + bx + c = 0 equation and just enter a, b, c!\n"
-           "a: ");
-
-    scanf("%lf", &a);
-
-    printf("b: ");
-    scanf("%lf", &b);
-
-    printf("c: ");
-    scanf("%lf", &c);
-
-    double root1 = 0, root2 = 0;
-
-    int nRoots = solveEquation(a, b, c, &root1, &root2);
-
-    printf("Your equation %gx^2 + %gx + %g = 0 has ", a, b, c);
-
-    if (nRoots == INFINITE_ROOTS)
-    {
-        printf("has infinite number of roots!\n"
-               "Any number will fit the equation!\n");
-    }
-    else if (nRoots == 1)
-    {
-        printf(" 1 root. It is %g\n", root1);
-    }
-    else
-    {
-        printf(" 2 roots. They are %g and %g\n", root1, root2);
-    }
-
-    printf("See you later!\n");
 }
